22e3bf1c3aa02def9d24a0d4f68f11928c58b7c0
ZayanIbneZia <ninjagamerz900@gmail.com>
2024-12-05 22:28:47 -0800

    Introduction to JavaScript

---
+++ script.js
@@ -0,0 +1,23 @@
+document.write("Starting JS...");
+console.log(3 + 4);
+// Variable
+var num1 = 20;
+var num2 = 10;
+var add = num1 + num2
+
+document.getElementById("add_result").innerHTML = add
+
+// Data Types
+var a = 300; //Number
+var b = 4.5; //Number
+var c = "JavaScript"; //String
+var d = false //Boolean
+
+// Student Info
+var student_name = "Shoccho";
+var student_grade = 6;
+var favourite_subject = "Maths";
+
+var student_information = `My name is ${student_name}.I study in grade ${student_grade}. My favoutite subject is ${favourite_subject}.`;
+
+document.getElementById("student_info").innerHTML = student_information;
